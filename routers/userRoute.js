765cbabaf32dfd13b40264d41d3462ec989aa3bb
Andika Dwi Abizar <andikabizar2004@gmail.com>
2023-06-19 15:52:34 +0700

    feature/forget-password

--- routers/userRoute.js
+++ routers/userRoute.js
@@ -4,11 +4,17 @@ const Router = express.Router();
 
 const { userController } = require("../controllers");
 
-const { verifyTokenEmail } = require("../auth/auth");
+const { verifyTokenEmail, verifyTokenForgetPassword } = require("../auth/auth");
 
 Router.post("/register", userController.createUser);
 Router.post("/login", userController.loginUser);
 Router.patch("/verify-email", verifyTokenEmail, userController.verifyEmail);
 Router.post("/resend-token-email", userController.resendEmailVerify);
+Router.post("/forget-pass-email", userController.sendForgetPassword);
+Router.patch(
+  "/password",
+  verifyTokenForgetPassword,
+  userController.verifyForgetPassword
+);
 
 module.exports = Router;
